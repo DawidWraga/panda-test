fc04014b42b089dbc8c3a20f537f50d3d2b1e93d
DawidWraga <dpwraga@gmail.com>
2023-08-05 20:23:20 +0100

    chore: remove styled system from git cache

--- src/ui/card.tsx
+++ src/ui/card.tsx
@@ -1,13 +1,67 @@
-import { css, cx } from '../../styled-system/css';
+import * as React from 'react';
+import { cx } from '@/styled-system/css';
+import {
+	card,
+	cardHeader,
+	cardTitle,
+	cardDescription,
+	cardContent,
+	cardFooter,
+} from '@/styled-system/recipes';
 
-const styles = css({
-	borderWidth: '1px',
-	borderRadius: '8px',
-	paddingX: '12px',
-	paddingY: '24px',
-});
+const Card = React.forwardRef<
+	HTMLDivElement,
+	React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+	<div ref={ref} className={cx(card(), className)} {...props} />
+));
+Card.displayName = 'Card';
 
-const Card = ({ className, ...props }: any) => {
-	const rootClassName = cx(styles, className);
-	return <div className={rootClassName} {...props} />;
+const CardHeader = React.forwardRef<
+	HTMLDivElement,
+	React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+	<div ref={ref} className={cx(cardHeader(), className)} {...props} />
+));
+CardHeader.displayName = 'CardHeader';
+
+const CardTitle = React.forwardRef<
+	HTMLParagraphElement,
+	React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+	<h3 ref={ref} className={cx(cardTitle(), className)} {...props} />
+));
+CardTitle.displayName = 'CardTitle';
+
+const CardDescription = React.forwardRef<
+	HTMLParagraphElement,
+	React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+	<p ref={ref} className={cx(cardDescription(), className)} {...props} />
+));
+CardDescription.displayName = 'CardDescription';
+
+const CardContent = React.forwardRef<
+	HTMLDivElement,
+	React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+	<div ref={ref} className={cx(cardContent(), className)} {...props} />
+));
+CardContent.displayName = 'CardContent';
+
+const CardFooter = React.forwardRef<
+	HTMLDivElement,
+	React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+	<div ref={ref} className={cx(cardFooter(), className)} {...props} />
+));
+CardFooter.displayName = 'CardFooter';
+
+export {
+	Card,
+	CardHeader,
+	CardFooter,
+	CardTitle,
+	CardDescription,
+	CardContent,
 };
